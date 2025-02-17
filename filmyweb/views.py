4baae9b8536765cd788954b8335302b633992b38
Konrad1640 <94623540+Konrad1640@users.noreply.github.com>
2025-02-17 18:52:32 +0100

    Update

--- filmyweb/views.py
+++ filmyweb/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from .models import Film
 from .forms import FilmForm
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def wszystkie_filmy(request):
     wszystkie = Film.objects.all()
     return render(request, 'filmy.html', {'filmy': wszystkie})
 
+@login_required
 def nowy_film(request):
     form = FilmForm(request.POST or None, request.FILES or None)
 
@@ -16,6 +19,7 @@ def nowy_film(request):
 
     return render(request, 'film_form.html', {'form': form})
 
+@login_required
 def edytuj_film(request, id):
     film = get_object_or_404(Film, pk=id)
     form = FilmForm(request.POST or None, request.FILES or None,
@@ -27,6 +31,7 @@ def edytuj_film(request, id):
 
     return render(request, 'film_form.html', {'form': form})
 
+@login_required
 def usun_film(request, id):
     film = get_object_or_404(Film, pk=id)
 
